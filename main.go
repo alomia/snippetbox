753c90912d5af71240a8832351c816c4dc8174d3
Alejandro Alomia <alejandroalomia730@gmail.com>
2023-01-14 15:01:32 -0500

    routing requests

--- main.go
+++ main.go
@@ -10,9 +10,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from Snippetbox")
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Display a specific snippet...")
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create a new snippet...")
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
