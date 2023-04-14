b354e77c24aaa1e974e7316204ee5907e4e9e95c
alomia <alejandroalomia730@gmail.com>
2023-04-14 18:04:49 -0500

    add new route create

--- main.go
+++ main.go
@@ -10,10 +10,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello from Snippetbox")
 }
 
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Create a new snippet...")
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
